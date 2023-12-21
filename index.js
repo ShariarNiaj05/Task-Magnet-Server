a0496ba9cbe2368043951c621a7f526093790ba2
Shariar Islam <shariarn85@gmail.com>
2023-12-21 22:31:49 +0600

    task posting api created

--- index.js
+++ index.js
@@ -1,11 +1,81 @@
+require('dotenv').config()
 const express = require('express')
+const cors = require('cors')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const app = express()
+const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+
+  ],
+  
+}))
+app.use(express.json());
 
 
-app.use(express.json())
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qiowubl.mongodb.net/?retryWrites=true&w=majority`;
+
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+const taskCollection = client.db('taskMagnetDB').collection('task')
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    // Send a ping to confirm a successful connection
+
+    app.put('/tasks', async (req, res) => {
+      const task = req.body;
+      const result = await taskCollection.insertOne(task)
+      res.send(result)
+    })
+
+
+    app.get('/tasks/:email', async (req, res) => {
+      const email = req.params.email;
+     
+      console.log('checking email',email);
+      // const filter = {email}
+
+      const result = await taskCollection.find().toArray();
+
+      res.send(result)
+    })
+
+
+    app.delete('/tasks/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await taskCollection.deleteOne(query)
+      res.send(result)
+
+    })
+
+    // app.get('/mycart', async (req, res) => {
+    //   const result = await cartCollection.find().toArray()
+    //   res.send(result)
+    // })
+
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 
 
